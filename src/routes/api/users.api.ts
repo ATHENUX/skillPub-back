7b0096eeb678f083aedd1c132d45d55a33869904
blopa2000 <jpagudelo2000@gmail.com>
2020-10-29 17:45:06 -0500

    Small changes

--- src/routes/api/users.api.ts
+++ src/routes/api/users.api.ts
@@ -4,7 +4,7 @@ import user from "controllers/users.controller";
 const router: Router = Router();
 
 //Access
-router.post("/signIn", [check("email").not().isEmpty().withMessage("Email is required"), check("password").not().isEmpty().withMessage("password is required")], user.signIn);
-router.post("/signUp", [check("email").not().isEmpty().withMessage("Email is required"), check("firstName").not().isEmpty().withMessage("Fist name is required"), check("lastName").not().isEmpty().withMessage("Last name is required"), check("password").not().isEmpty().withMessage("password is required")], user.signUp);
+router.post("/signIn", [check("email").not().isEmpty().withMessage("Email is required"), check("password").not().isEmpty().withMessage("Password is required")], user.signIn);
+router.post("/signUp", [check("email").not().isEmpty().withMessage("Email is required"), check("firstName").not().isEmpty().withMessage("First name is required"), check("lastName").not().isEmpty().withMessage("Last name is required"), check("password").not().isEmpty().withMessage("password is required")], user.signUp);
 
 export default router;
